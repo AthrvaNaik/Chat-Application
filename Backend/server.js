818486515636f5bc7625e1214772f257ab6c9b2f
athrvanaik <athrvanaik227@gmail.com>
2025-01-12 21:54:32 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+import path from 'path';
 
 import express from "express";
 import authRoutes from "./Routes/auth.routes.js";
@@ -9,6 +10,9 @@ import connectDB from "./DB/connect.js";
 import cookieParser from "cookie-parser";
 
 import { app as socketApp, server, io } from "./Socket/socket.js"; // Rename 'app' to 'socketApp'
+
+const PORT = process.env.PORT || 8000;
+const __dirname=path.resolve()
 
 const app = express(); // No conflict now
 
@@ -24,7 +28,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 8000;
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend","dist","index.html"));
+})
+
 
 // Start the server
 app.listen(PORT, () => {
