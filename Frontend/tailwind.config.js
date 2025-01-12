f347506daf5faa5013ade948511aa86c755f6f3c
athrvanaik <athrvanaik227@gmail.com>
2025-01-12 22:09:08 +0530

    nn

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-	content: [
-	  "./index.html",
-	  "./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-	  extend: {},
-	},
-	plugins: [
-    // eslint-disable-next-line no-undef
-    require('daisyui'),
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  }
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+};
